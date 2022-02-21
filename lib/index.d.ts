1da3a36484f773e3217199029eb1f5d2c8e64ee1
Alex Fuentes <alfupe@gmail.com>
2022-02-21 16:50:39 +0100

    add testing

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare type FactoryOptions = {
-    direction?: string;
-};
-declare type Data = object[] | object;
-export declare function mutatorFactory(data: Data, mutators?: any[], options?: FactoryOptions): any;
-export {};
---
+++ src/index.test.js
@@ -0,0 +1,111 @@
+import { mutatorFactory } from './index';
+
+const addSurname = (data) => ({
+  ...data,
+  surname: data.name.split('').reverse().join(''),
+});
+const addAge = (data) => {
+  return {
+    ...data,
+    age:
+      data?.name && data?.surname
+        ? `${(data.name + data.surname).length}`
+        : 'unknown',
+  };
+};
+
+test('should transform and array of data piping the mutators', () => {
+  const testData = [
+    {
+      name: 'Foo',
+    },
+    {
+      name: 'Bar',
+    },
+  ];
+  const received = mutatorFactory(testData, [addSurname, addAge]);
+  expect(received).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "age": "6",
+        "name": "Foo",
+        "surname": "ooF",
+      },
+      Object {
+        "age": "6",
+        "name": "Bar",
+        "surname": "raB",
+      },
+    ]
+  `);
+});
+
+test('should transform an array of data composing the mutators', () => {
+  const testData = [
+    {
+      name: 'Foo',
+    },
+    {
+      name: 'Bar',
+    },
+  ];
+  const received = mutatorFactory(testData, [addAge, addSurname], {
+    direction: 'rtl',
+  });
+  expect(received).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "age": "6",
+        "name": "Foo",
+        "surname": "ooF",
+      },
+      Object {
+        "age": "6",
+        "name": "Bar",
+        "surname": "raB",
+      },
+    ]
+  `);
+});
+
+test('should transform the array data composing the mutators with unknown age due to the incorrect mutator order', () => {
+  const testData = [
+    {
+      name: 'Foo',
+    },
+    {
+      name: 'Bar',
+    },
+  ];
+  const received = mutatorFactory(testData, [addSurname, addAge], {
+    direction: 'rtl',
+  });
+  expect(received).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "age": "unknown",
+        "name": "Foo",
+        "surname": "ooF",
+      },
+      Object {
+        "age": "unknown",
+        "name": "Bar",
+        "surname": "raB",
+      },
+    ]
+  `);
+});
+
+test('should transform an object data piping the mutators', () => {
+  const testData = {
+    name: 'Foo',
+  };
+  const received = mutatorFactory(testData, [addSurname, addAge]);
+  expect(received).toMatchInlineSnapshot(`
+    Object {
+      "age": "6",
+      "name": "Foo",
+      "surname": "ooF",
+    }
+  `);
+});
